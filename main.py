17c74a0f4609d9fa374309105c4b579faf227911
AnnaLipenberga <124579305+AnnaLipenberga@users.noreply.github.com>
2023-02-17 12:48:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,20 +13,21 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
             opening_brackets_stack.append(Bracket(next, i + 1))
+            # Process opening bracket, write your code here
+            
             
 
         if next in ")]}":
-            # Process closing bracket, write your code here
             if not opening_brackets_stack  or  not are_matching(opening_brackets_stack[-1].char, next):
-               return i+1
+                return i+1
             opening_brackets_stack.pop()
-    if opening_brackets_stack:
-      return opening_brackets_stack[-1].position
-    return "Success"
-                
+            if opening_brackets_stack:
+                return opening_brackets_stack[-1].position
+            return "Success"
+            # Process closing bracket, write your code here
             
+          
 
 
 
