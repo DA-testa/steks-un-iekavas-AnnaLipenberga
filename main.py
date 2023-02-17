2f94b8eb98b91290be55aa316e07b348755fdb1d
AnnaLipenberga <124579305+AnnaLipenberga@users.noreply.github.com>
2023-02-17 20:15:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def find_mismatch(text):
             opening_brackets_stack.pop()
         if opening_brackets_stack:
             return opening_brackets_stack[-1].position
-        return "Success"
+            return "Success"
             
                 
             
